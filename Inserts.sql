05814c74776ceadd86316109c6d2e42822da5d04
Caio Santos <caiosantosfreitas@hotmail.com>
2019-11-03 15:57:37 -0300

    Add files via upload

---
+++ Inserts.sql
@@ -0,0 +1,7 @@
+INSERT INTO `aluno` VALUES (1,'caio@gmail.com','Caio'),(2,'andre@gmail.com','Andre'),(3,'rogerio@gmail.com','Rogerio'),(4,'maleck@gmail.com','Maleck'),(5,'kenya@gmail.com','Kenya'),(6,'juca@gmail.com','Juca'),(7,'mario@gmail.com','Mario'),(8,'jorge@gmail.com','Jorge'),(9,'afonso@gmail.com','Afonso'),(10,'afonso@gmail.com','Edson'),(11,'amanda@gmail.com','Amanda'),(12,'gessica@gmail.com','Gessica');
+INSERT INTO `professor` VALUES (1,'joaquim@gmail.com','Joaquim das Neves',13),(2,'alberto@gmail.com','Alberto Correa',324);
+INSERT INTO `curso` VALUES (1,'Ensino Médio');
+INSERT INTO `materia` VALUES (1,'Geografia',6,2),(2,'História',8,1);
+INSERT INTO `turma` VALUES (1,'Turma A Diurno',1),(2,'Turma B Noturno',1);
+INSERT INTO `aluno_turmas` VALUES (1,1),(2,1),(3,1),(4,1),(5,1),(6,1),(7,1),(8,2),(9,2),(10,2),(11,2),(12,2);
+INSERT INTO `curso_materias` VALUES (1,1),(1,2);
